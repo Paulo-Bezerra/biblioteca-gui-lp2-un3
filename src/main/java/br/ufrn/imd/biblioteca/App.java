92f15bf78a6733eb54432fef6e928f0c48c33b42
Paulo Bezerra <43281474+Paulo-Bezerra@users.noreply.github.com>
2024-06-21 14:03:42 -0300

    Atualiza o ícone da aplicação.

--- src/main/java/br/ufrn/imd/biblioteca/App.java
+++ src/main/java/br/ufrn/imd/biblioteca/App.java
@@ -3,11 +3,13 @@ package br.ufrn.imd.biblioteca;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class App extends Application {
+  @SuppressWarnings("exports")
   @Override
   public void start(Stage stage) throws IOException {
     FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view/login.fxml"));
@@ -15,6 +17,7 @@ public class App extends Application {
     stage.setMinWidth(280);
     stage.setMinHeight(390);
     stage.setTitle("Biblioteca");
+    stage.getIcons().add(new Image(getClass().getResourceAsStream("images/App-256.png")));
     stage.setScene(scene);
     stage.show();
   }
