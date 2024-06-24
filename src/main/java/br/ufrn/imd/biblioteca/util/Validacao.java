8b04334133286652c099bc4bb80d435926606b95
Paulo-Bezerra <43281474+Paulo-Bezerra@users.noreply.github.com>
2024-06-24 15:24:14 -0300

    Atualiza nome da validação de Strings numéricas.

--- src/main/java/br/ufrn/imd/biblioteca/util/Validacao.java
+++ src/main/java/br/ufrn/imd/biblioteca/util/Validacao.java
@@ -2,7 +2,7 @@ package br.ufrn.imd.biblioteca.util;
 
 public class Validacao {
   // Método para verificar se uma string contém apenas números
-  public static boolean isNumeric(String str) {
+  public static boolean strNumerica(String str) {
     return str.matches("\\d+");
   }
 }
