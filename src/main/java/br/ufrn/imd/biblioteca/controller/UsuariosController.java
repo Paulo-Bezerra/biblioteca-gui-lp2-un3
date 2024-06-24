81750f7297b93ead30f086ebb11016ef6bfae4bb
Paulo-Bezerra <43281474+Paulo-Bezerra@users.noreply.github.com>
2024-06-24 16:18:25 -0300

    Adiciona listar e buscar usuário.

--- src/main/java/br/ufrn/imd/biblioteca/controller/UsuariosController.java
+++ src/main/java/br/ufrn/imd/biblioteca/controller/UsuariosController.java
@@ -1,11 +1,24 @@
 package br.ufrn.imd.biblioteca.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 import br.ufrn.imd.biblioteca.App;
+import br.ufrn.imd.biblioteca.dto.UsuarioDTO;
+import br.ufrn.imd.biblioteca.service.OperacoesUsuarios;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class UsuariosController {
+
+  @FXML
+  private TextField tfBusca;
+
+  @FXML
+  private ListView<UsuarioDTO> lvUsuarios;
   @FXML
   private void cadastrarUsuario() throws IOException {
     App.trocarTela("cadastrar-usuario");
@@ -25,5 +38,27 @@ public class UsuariosController {
   @FXML
   private void sair() throws IOException {
     App.trocarTela("login");
+  }
+
+  @FXML
+  private void listarUsarios() {
+    List<UsuarioDTO> usuarios = OperacoesUsuarios.listarUsuarios();
+    usuarios.sort(Comparator.comparing(UsuarioDTO::nome));
+    lvUsuarios.getItems().setAll(usuarios);
+  }
+
+  @FXML
+  private void buscarUsuario() {
+    if (tfBusca.getText().trim().isEmpty()) {
+      return;
+    }
+    List<UsuarioDTO> usuarios = new ArrayList<>();
+    for (UsuarioDTO usuario : OperacoesUsuarios.listarUsuarios()) {
+      if (usuario.nome().matches(".*" + tfBusca.getText().trim() + ".*")) {
+        usuarios.add(usuario);
+      }
+    }
+    usuarios.sort(Comparator.comparing(UsuarioDTO::nome));
+    lvUsuarios.getItems().setAll(usuarios);
   }
 }
