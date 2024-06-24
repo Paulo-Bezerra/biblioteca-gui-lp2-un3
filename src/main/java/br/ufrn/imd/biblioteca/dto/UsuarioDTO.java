590a1cd2d13a9dca2c6372cc8f2028eb99981826
Paulo-Bezerra <43281474+Paulo-Bezerra@users.noreply.github.com>
2024-06-24 16:15:02 -0300

    Formata o toString() do UsuarioDTO.

--- src/main/java/br/ufrn/imd/biblioteca/dto/UsuarioDTO.java
+++ src/main/java/br/ufrn/imd/biblioteca/dto/UsuarioDTO.java
@@ -1,6 +1,8 @@
 package br.ufrn.imd.biblioteca.dto;
 
-public record UsuarioDTO() {
-  static String nome;
-  static String matricula;
+public record UsuarioDTO(String nome, String matricula) {
+  @Override
+  public String toString() {
+    return "Nome: " + nome + ", Matricula: " + matricula;
+  }
 }
