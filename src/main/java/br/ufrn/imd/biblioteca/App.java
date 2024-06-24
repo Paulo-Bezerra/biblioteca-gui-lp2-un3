a1023ebecedb8a97a66af90648ffd234fcd4c50c
Paulo-Bezerra <43281474+Paulo-Bezerra@users.noreply.github.com>
2024-06-24 16:12:26 -0300

    Adiciona a persistência dos dados.

--- src/main/java/br/ufrn/imd/biblioteca/App.java
+++ src/main/java/br/ufrn/imd/biblioteca/App.java
@@ -1,5 +1,6 @@
 package br.ufrn.imd.biblioteca;
 
+import br.ufrn.imd.biblioteca.dao.BancoDAO;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -35,6 +36,8 @@ public class App extends Application {
   }
 
   public static void main(String[] args) {
+    BancoDAO.getInstance().carregarDados();
     launch();
+    BancoDAO.getInstance().salvarDados();
   }
 }
