a48866d569b628617b9d41d495ae58ec55010ec1
Paulo-Bezerra <43281474+Paulo-Bezerra@users.noreply.github.com>
2024-06-24 22:47:29 -0300

    Cria a classe com métodos para tratar Strings.

---
+++ src/main/java/br/ufrn/imd/biblioteca/util/Tratamento.java
@@ -0,0 +1,17 @@
+package br.ufrn.imd.biblioteca.util;
+
+import java.text.Normalizer;
+
+public class Tratamento {
+  // Método para verificar se uma string contém outra, ignorando acentos e capitalização.
+  public static boolean contemString(String str, String subStr) {
+    return removerAcentos(str).toLowerCase().matches(".*" + removerAcentos(subStr).toLowerCase() + ".*");
+  }
+
+  // Método auxiliar para remover acentos de uma string
+  public static String removerAcentos(String str) {
+    // Normaliza e remove os acentos
+    return Normalizer.normalize(str, Normalizer.Form.NFD)
+      .replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
+  }
+}
